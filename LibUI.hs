7471a138e167e4ef4337674f661e29d349c37cf3
alexmcmil <77553190+alexmcmil@users.noreply.github.com>
2021-03-07 11:07:00 -0800

    Update LibUI.hs

--- LibUI.hs
+++ LibUI.hs
@@ -2,8 +2,12 @@ module LibUI where
 
 ---ABOUT: Library for formatters in main
 
+
+---basic code courtesy of stack overflow
+---- helper for removing all non-letter ASCII characters from plaintext input 
 removenonLetters :: [Char] -> [Char]
 removenonLetters message = filter (not . (`elem` ",.<> ?!-@#$%^&*1234567890()_+=|{}[]~:;\"\'")) message
 
+---same but keeps numerical
 removeSpecial:: [Char] -> [Char]
 removeSpecial message = filter (not . (`elem` ",.<> ?!-@#$%^&*()_+=|{}[]~:;\"\'")) message
