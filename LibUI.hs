9aa40e580cf6263a24208ff80431c3e0817510f1
alexmcmil <77553190+alexmcmil@users.noreply.github.com>
2021-03-07 13:16:31 -0800

    Update LibUI.hs

--- LibUI.hs
+++ LibUI.hs
@@ -1,7 +1,12 @@
 module LibUI where
 
+import Data.List as L
+import Data.Char as Ch
+
 ---ABOUT: Library for formatters in main
 
+formatKeyBasic :: [Char] -> [Char]
+formatKeyBasic key = L.map Ch.toLower(removenonLetters key)
 
 ---basic code courtesy of stack overflow
 ---- helper for removing all non-letter ASCII characters from plaintext input 
