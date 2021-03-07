9a5fe729d1ff05bd30b5563d17625b0c63a3c849
alexmcmil <77553190+alexmcmil@users.noreply.github.com>
2021-03-07 11:49:25 -0800

    Update LibDataForm.hs

--- LibDataForm.hs
+++ LibDataForm.hs
@@ -16,3 +16,39 @@ module LibDataForm where
 
 ----Note: only a few current functions work for 3x3 matrixes and 1x3 vectors, most do not and therefore for now 
 ----this should only be considered as 2x2 and 1x2 usage only
+
+---data format helpers (see readme)
+
+---char list to matrix
+charKeytoMatrix :: [Char] -> [[Int]]
+charKeytoMatrix list = intListtoMatrix (messageNum list) 
+
+---will need to be sure is 2x2 or 4 long, but takes an integer list and converts to a 2x2 matrix
+intListtoMatrix :: [Int] -> [[Int]]
+intListtoMatrix list = [(T.fst (splitAt 2 list)), (T.snd (splitAt 2 list))]
+
+-----basic converter for a list to a list of 1x2 vectors
+---example of arg to use: listVect [1,2..14] 1 [[[]]]
+listVect :: [Int] -> Int -> [[[Int]]] -> [[[Int]]]
+listVect list iter vectors
+ | (iter > (div (length list) 2)) = reverse (take ((length vectors) - 1) vectors)
+ | (iter <= (div (length list) 2)) = listVect list (iter + 1) ((changeVectAT list iter) : vectors) 
+ 
+ ---basic converter from a list of 1x2 vectors back to a plain list
+ ----example of arg to use: changeVecttoList a [] 0
+changeVecttoList :: [[[Int]]] -> [Int] -> Int -> [Int]
+changeVecttoList vectors newlist iter
+ | (iter >= (length vectors)) = newlist
+ | (iter < (length vectors)) = changeVecttoList vectors (L.concat [newlist, (L.concat [vectors !! iter !! 0, vectors !! iter !! 1])]) (iter + 1)
+ 
+
+----helpers for working with vectors
+
+changeVectAT :: [Int] -> Int -> [[Int]]
+changeVectAT list iter = [[(getTwoElem list iter) !! 0], [(getTwoElem list iter) !! 1]]
+
+getTwoElem :: [Int] -> Int -> [Int]
+getTwoElem list iter
+  | (iter == 1) = take 2 list
+  | (iter > 1) = drop ((iter * 2) -2) (take (iter * 2) list)
+  | (iter < 1) = [-1]
