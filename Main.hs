fc302698f2e0b96ef1a3be9681a44fc824100f4e
zachj555 <zachj555@users.noreply.github.com>
2021-03-07 12:14:26 -0800

    update main to current version

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List as L
 import LibHill as LH
 import System.Exit as E
 
@@ -10,30 +11,50 @@ main = do
    let msgMatrix = (LH.formatMFin initialMsg)
    putStrLn "Message after padding: "
    print msgMatrix
-   putStrLn "\nWould you like to encrypt('e') or decrypt('d'): "
-   choice <- getLine
-   if (choice == "e")
+   putStrLn "Input a key - the key must be 4 characters long: "
+   initialKey <- getLine
+   let tempKeyMatrix = (LH.formatMFin initialKey)
+   if ((L.length tempKeyMatrix) /= 4)
       then do
-         ---do encryption stuff
-         putStrLn "Your encrypted message is: "
-         ---output the message
-         putStrLn "\nPress y to continue, or any other key to exit: "
-         continue <-getLine
-         if (continue == "y")
-            then main
-         else
-            E.exitSuccess
-   else if (choice == "d")
-      then do
-         ---do decryption stuff
-         putStrLn "Your decrypted message is: "
-         ---output the message
-         putStrLn "\nPress y to continue, or any other key to exit: "
-         continue <-getLine
-         if (continue == "y")
-            then main
-         else
-            E.exitSuccess
+         putStrLn "ERROR: Key is improper length, returning to top."
+         main
    else do
-      putStrLn " ERROR: Invalid choice, returning to top!"
-      main
+      ---do decryption key generation
+      ---do check for if decryption key returned error
+      ---if ((checkMatrix decyrKey))
+      ---let decyrKey = calcDKeyTwos (intListtoMatrix (messageNum tempKeyMatrix))
+      ---output decryption key --- in matrix form?
+      putStrLn "\nWould you like to encrypt('e') or decrypt('d'): "
+      choice <- getLine
+      if (choice == "e")
+         then do
+            ---do encryption stuff
+            putStrLn "Your encrypted message is: "
+            ---output the message
+            putStrLn "\nPress y to continue, or any other key to exit: "
+            continue <-getLine
+            if (continue == "y")
+               then main
+            else
+               E.exitSuccess
+      else if (choice == "d")
+         then do
+            ---do decryption stuff
+            putStrLn "Your decrypted message is: "
+            ---output the message
+            putStrLn "\nPress y to continue, or any other key to exit: "
+            continue <-getLine
+            if (continue == "y")
+               then main
+            else
+               E.exitSuccess
+      else do
+         putStrLn " ERROR: Invalid choice, returning to top!"
+         main
+
+outputMatrix :: [Char] -> IO()
+outputMatrix m = do
+  let work = [[m!!0],[m!!1],[m!!2],[m!!3]]
+  putStrLn "Your key is: "
+  putStrLn ("|" ++ (work!!0) ++ " " ++ (work!!1) ++ "|")
+  putStrLn ("|" ++ (work!!2) ++ " " ++ (work!!3) ++ "|")
