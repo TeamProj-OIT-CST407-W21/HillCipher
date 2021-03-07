87b76db33887a4f664016203706a4ba9f930513b
alexmcmil <77553190+alexmcmil@users.noreply.github.com>
2021-03-07 11:03:27 -0800

    Update LibUI.hs

--- LibUI.hs
+++ LibUI.hs
@@ -1,39 +1,9 @@
 module LibUI where
 
-import LibHill as LH
-import System.Exit as E
+---ABOUT: Library for formatters in main
 
-LibUI :: IO()
-LibUI = do
-   putStrLn "Enter a message to en/decrypt: "
-   initialMsg <- getLine
-   let msgMatrix = (LH.formatMFin initialMsg)
-   putStrLn "Message after padding: "
-   print msgMatrix
-   putStrLn "\nWould you like to encrypt('e') or decrypt('d'): "
-   choice <- getLine
-   if (choice == "e")
-      then do
-         ---do encryption stuff
-         putStrLn "Your encrypted message is: "
-         ---output the message
-         putStrLn "\nPress y to continue, or any other key to exit: "
-         continue <-getLine
-         if (continue == "y")
-            then LibUI
-         else
-            E.exitSuccess
-   else if (choice == "d")
-      then do
-         ---do decryption stuff
-         putStrLn "Your decrypted message is: "
-         ---output the message
-         putStrLn "\nPress y to continue, or any other key to exit: "
-         continue <-getLine
-         if (continue == "y")
-            then LibUI
-         else
-            E.exitSuccess
-   else do
-      putStrLn " ERROR: Invalid choice, returning to top!"
-      LibUI
+removenonLetters :: [Char] -> [Char]
+removenonLetters message = filter (not . (`elem` ",.<> ?!-@#$%^&*1234567890()_+=|{}[]~:;\"\'")) message
+
+removeSpecial:: [Char] -> [Char]
+removeSpecial message = filter (not . (`elem` ",.<> ?!-@#$%^&*()_+=|{}[]~:;\"\'")) message
